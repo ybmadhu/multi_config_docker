a9e68234e432fe6d1d9bd1cee71240e848bc5d0c
Ubuntu <ubuntu@ip-172-31-13-199.ap-south-1.compute.internal>
2019-09-18 08:52:38 +0000

    message

---
+++ latest/App.java
@@ -0,0 +1,11 @@
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World .. madhu JMS technologies." );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+}
